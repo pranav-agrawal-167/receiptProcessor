5d646fe52b6c1cbc96698815eb590fd720a0dd8d
pranavagrawal <psagrawa@asu.edu>
2023-06-26 03:07:35 -0700

    Added unit tests

---
+++ main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type Item struct {
+	ShortDescription string  `json:"shortDescription"`
+	Price            float64 `json:"price,string"`
+}
+
+type ReceiptPurchaseDate time.Time
+
+type ReceiptPurchaseTime time.Time
+
+type Receipt struct {
+	Retailer     string              `json:"Retailer"`
+	PurchaseDate ReceiptPurchaseDate `json:"purchaseDate"`
+	PurchaseTime ReceiptPurchaseTime `json:"purchaseTime"`
+	Items        []Item              `json:"items"`
+	Total        float64             `json:"total,string"`
+}
+
+var receiptMap = make(map[string]Receipt)
+
+func main() {
+	router := gin.Default()
+	receiptsGroup := router.Group("/receipts")
+
+	receiptsGroup.POST("/process", PostReceipt)
+	receiptsGroup.GET("/:id/points", GetPoints)
+
+	router.Run(":8080")
+}
+
+func PostReceipt(c *gin.Context) {
+	var receipt Receipt
+
+	if err := c.BindJSON(&receipt); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	receiptId := uuid.New().String()
+	receiptMap[receiptId] = receipt
+
+	c.JSON(http.StatusOK, gin.H{"id": receiptId})
+}
+
+func GetPoints(c *gin.Context) {
+	receiptId := c.Param("id")
+	receipt, ok := receiptMap[receiptId]
+	if ok {
+		points := CalculatePoints(&receipt)
+		c.JSON(http.StatusOK, gin.H{"points": points})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+	}
+}
+
+func CalculatePoints(receipt *Receipt) int {
+	var pointsRewarded = 0
+
+	pointsRewarded += CalculateRetailerNamePoints(receipt)
+
+	pointsRewarded += AddRoundDollarPoints(receipt)
+
+	pointsRewarded += AddQuarterDollarPoints(receipt)
+
+	pointsRewarded += CalculatePurchaseItemsPoints(receipt)
+
+	pointsRewarded += CalculateItemDescriptionPoints(receipt)
+
+	pointsRewarded += AddOddPurchaseDayPoints(receipt)
+
+	pointsRewarded += AddPurchaseTimePoints(receipt)
+
+	return pointsRewarded
+}
+
+func CalculateRetailerNamePoints(receipt *Receipt) int {
+	var count = 0
+	if receipt.Retailer != "" {
+		// Rule 1: One point for each alphanumeric character in retailer name
+		for _, char := range receipt.Retailer {
+			if unicode.IsLetter(char) || unicode.IsDigit(char) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func AddRoundDollarPoints(receipt *Receipt) int {
+	// Rule 2: 50 points if the total is a round dollar amount with no cents.
+	if math.Mod(receipt.Total, 1) == 0 {
+		return 50
+	}
+	return 0
+}
+
+func AddQuarterDollarPoints(receipt *Receipt) int {
+	// Rule 3: 25 points if the total is a multiple of 0.25.
+	if math.Mod(receipt.Total, 0.25) == 0 {
+		return 25
+	}
+	return 0
+}
+
+func CalculatePurchaseItemsPoints(receipt *Receipt) int {
+	// Rule 4: 5 points for every two items on the receipt.
+	if receipt.Items != nil {
+		return len(receipt.Items) / 2 * 5
+	}
+	return 0
+}
+
+func CalculateItemDescriptionPoints(receipt *Receipt) int {
+	// Rule 5: If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
+	var earnedPoints = 0
+	if receipt.Items != nil {
+		for _, item := range receipt.Items {
+			if item.ShortDescription != "" {
+				if len(strings.TrimSpace(item.ShortDescription))%3 == 0 {
+					earnedPoints += int(math.Ceil(item.Price * 0.2))
+				}
+			}
+		}
+	}
+	return earnedPoints
+}
+
+func AddOddPurchaseDayPoints(receipt *Receipt) int {
+	// Rule 6: 6 points if the day in the purchase date is odd.
+	day := time.Time(receipt.PurchaseDate).Day()
+	if day%2 != 0 {
+		return 6
+	}
+	return 0
+}
+
+func AddPurchaseTimePoints(receipt *Receipt) int {
+	// Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+	purTime := time.Time(receipt.PurchaseTime)
+	startTime := time.Date(purTime.Year(), purTime.Month(), purTime.Day(), 14, 0, 0, 0, purTime.Location()) // 2:00 PM
+	endTime := time.Date(purTime.Year(), purTime.Month(), purTime.Day(), 16, 0, 0, 0, purTime.Location())   // 4:00 PM
+
+	if purTime.After(startTime) && purTime.Before(endTime) {
+		return 10
+	}
+	return 0
+}
+
+func (purchaseDate *ReceiptPurchaseDate) UnmarshalJSON(b []byte) error {
+	trimmedDateString := strings.Trim(string(b), "\"")
+	formattedDate, err := time.Parse("2006-01-02", trimmedDateString)
+	if err != nil {
+		return fmt.Errorf("failed to parse ReceiptPurchaseDate: %w", err)
+	}
+	*purchaseDate = ReceiptPurchaseDate(formattedDate)
+	return nil
+}
+
+func (purchaseTime *ReceiptPurchaseTime) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	formattedTime, err := time.Parse("15:04", s)
+	if err != nil {
+		return fmt.Errorf("failed to parse ReceiptPurchaseTime: %w", err)
+	}
+	*purchaseTime = ReceiptPurchaseTime(formattedTime)
+	return nil
+}
---
+++ main_test.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func ReceiptsforTesting() []Receipt {
+	var testReceipts [3]Receipt
+	testReceipts[0] = Receipt{
+		Retailer:     "ABC Supermarket",
+		PurchaseDate: ReceiptPurchaseDate(time.Date(2023, time.January, 1, 15, 0, 0, 0, time.UTC)),
+		PurchaseTime: ReceiptPurchaseTime(time.Date(2023, time.January, 1, 15, 0, 0, 0, time.UTC)),
+		Items: []Item{
+			{ShortDescription: "Item 1", Price: 9.99},
+			{ShortDescription: "Item 2", Price: 5.99},
+			{ShortDescription: "", Price: 3.49},
+		},
+		Total: 19.47,
+	}
+	testReceipts[1] = Receipt{
+		Retailer:     "",
+		PurchaseDate: ReceiptPurchaseDate(time.Date(2023, time.January, 2, 14, 0, 0, 0, time.UTC)),
+		PurchaseTime: ReceiptPurchaseTime(time.Date(2023, time.January, 2, 14, 0, 0, 0, time.UTC)),
+		Items: []Item{
+			{ShortDescription: "Apples   ", Price: 11.65},
+			{ShortDescription: "Bananas", Price: 3.35},
+		},
+		Total: 15,
+	}
+	testReceipts[2] = Receipt{
+		Retailer:     "Barnes & Noble 60131",
+		PurchaseDate: ReceiptPurchaseDate(time.Date(2023, time.January, 10, 14, 10, 0, 0, time.UTC)),
+		PurchaseTime: ReceiptPurchaseTime(time.Date(2023, time.January, 10, 14, 10, 0, 0, time.UTC)),
+		Items:        nil,
+		Total:        12.25,
+	}
+	return testReceipts[:]
+}
+
+func TestPostReceipt(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/receipts/process", PostReceipt)
+
+	receiptData := map[string]interface{}{
+		"retailer":     "ABC",
+		"purchaseDate": "2022-01-01",
+		"purchaseTime": "17:52",
+		"items": []map[string]interface{}{
+			{
+				"shortDescription": "Testing data",
+				"price":            "11.11",
+			},
+		},
+		"total": "35.35",
+	}
+
+	jsonData, _ := json.Marshal(receiptData)
+
+	req, err := http.NewRequest("POST", "/receipts/process", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	var actualResponse map[string]string
+	error := json.Unmarshal(recorder.Body.Bytes(), &actualResponse)
+	if error != nil {
+		t.Error("Error in parsing the JSON: ", error)
+	}
+
+	receivedUUID := actualResponse["id"]
+	decodedUUID, uuidError := uuid.Parse(receivedUUID)
+	fmt.Print(decodedUUID)
+
+	if uuidError != nil {
+		t.Error("Generated response is not a valid UUID: ", uuidError)
+	}
+}
+
+func TestGetPoints_withValidId(t *testing.T) {
+	// create a mock receipt
+	gin.SetMode(gin.TestMode)
+	testReceipt := ReceiptsforTesting()[0]
+	// add mock receipt to map
+	receiptMap["validID"] = testReceipt
+	// call GetPoints with valid ID
+	router := gin.Default()
+	router.GET("/:id/points", GetPoints)
+	reqValidID, err := http.NewRequest("GET", "/validID/points", nil)
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
+	// check if status is 200
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, reqValidID)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	// remove validID entry from map
+	delete(receiptMap, "validID")
+}
+
+func TestGetPoints_withInvalidId(t *testing.T) {
+	// call GetPoints with valid ID
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.GET("/:id/points", GetPoints)
+	reqInvalidID, err := http.NewRequest("GET", "/invalidID/points", nil)
+	// check if status is 200
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, reqInvalidID)
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
+}
+
+func TestCalculateRetailerNamePoints(t *testing.T) {
+	testReceipts := ReceiptsforTesting()
+	expectedResponse := [3]int{14, 0, 16}
+	var actualResponse [3]int
+	for i := 0; i < 3; i++ {
+		actualResponse[i] = CalculateRetailerNamePoints(&testReceipts[i])
+	}
+	assert.Equal(t, expectedResponse, actualResponse)
+}
+
+func TestAddRoundDollarPoints(t *testing.T) {
+	testReceipts := ReceiptsforTesting()
+	expectedResponse := [3]int{0, 50, 0}
+	var actualResponse [3]int
+	for i := 0; i < 3; i++ {
+		actualResponse[i] = AddRoundDollarPoints(&testReceipts[i])
+	}
+	assert.Equal(t, expectedResponse, actualResponse)
+}
+
+func TestAddQuarterDollarPoints(t *testing.T) {
+	testReceipts := ReceiptsforTesting()
+	expectedResponse := [3]int{0, 25, 25}
+	var actualResponse [3]int
+	for i := 0; i < 3; i++ {
+		actualResponse[i] = AddQuarterDollarPoints(&testReceipts[i])
+	}
+	assert.Equal(t, expectedResponse, actualResponse)
+}
+
+func TestCalculatePurchaseItemsPoints(t *testing.T) {
+	testReceipts := ReceiptsforTesting()
+	expectedResponse := [3]int{5, 5, 0}
+	var actualResponse [3]int
+	for i := 0; i < 3; i++ {
+		actualResponse[i] = CalculatePurchaseItemsPoints(&testReceipts[i])
+	}
+	assert.Equal(t, expectedResponse, actualResponse)
+}
+
+func TestCalculateItemDescriptionPoints(t *testing.T) {
+	testReceipts := ReceiptsforTesting()
+	expectedResponse := [3]int{4, 3, 0}
+	var actualResponse [3]int
+	for i := 0; i < 3; i++ {
+		actualResponse[i] = CalculateItemDescriptionPoints(&testReceipts[i])
+	}
+	assert.Equal(t, expectedResponse, actualResponse)
+}
+
+func TestAddOddPurchaseDayPoints(t *testing.T) {
+	testReceipts := ReceiptsforTesting()
+	expectedResponse := [3]int{6, 0, 0}
+	var actualResponse [3]int
+	for i := 0; i < 3; i++ {
+		actualResponse[i] = AddOddPurchaseDayPoints(&testReceipts[i])
+	}
+	assert.Equal(t, expectedResponse, actualResponse)
+}
+
+func TestAddPurchaseTimePoints(t *testing.T) {
+	testReceipts := ReceiptsforTesting()
+	expectedResponse := [3]int{10, 0, 10}
+	var actualResponse [3]int
+	for i := 0; i < 3; i++ {
+		actualResponse[i] = AddPurchaseTimePoints(&testReceipts[i])
+	}
+	assert.Equal(t, expectedResponse, actualResponse)
+}
